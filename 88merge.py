aba100a245a49febfca6d5816e7e227b08fa559f
Aleksey Pedan <35327043+alexspedan@users.noreply.github.com>
2024-04-08 22:51:22 +0400

    Update 88merge.py
    add solution

--- 88merge.py
+++ 88merge.py
@@ -14,3 +14,11 @@ Output: [1,2,2,3,5,6]
 Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
 The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
 """
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        del nums1[m:]
+        nums1.extend(nums2)
+        nums1.sort()
